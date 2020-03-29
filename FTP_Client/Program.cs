f03d952cd4ca3085b700628d37e346b009a68c99
AlexandreBoissiere <alexgf27550@gmail.com>
2020-03-29 13:17:45 +0200

    Ajout de fichiers de base.

---
+++ FTP_Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static Total_library.Network.Com;
+
+namespace FTP_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            int initialCount = client.got.Count;
+            string serverIP = null;
+            Console.WriteLine("Server IP to connect : ");
+            serverIP = Console.ReadLine();
+
+            while (true)
+            {
+                if (client.got.Count > initialCount)
+                {
+                    string received = Encoding.ASCII.GetString(client.got[0].Data);
+                    if (received.Contains("READCONTENT_"))
+                    {
+                        received = received.Remove(0, 12);
+                        Console.WriteLine($"Content : {received}");
+                        client.got.RemoveAt(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine(received);
+                        client.got.RemoveAt(0);
+                    }
+                }
+
+                Console.WriteLine("mode to use (write, read, append) : ");
+                string smode = Console.ReadLine();
+                Console.WriteLine("File to use : ");
+                string sfile = Console.ReadLine();
+                
+                if (smode == "read")
+                {
+                    string mode = "R";
+                    string sent = $"{mode}{sfile}";
+
+                    client.Send(sent, serverIP);
+                }
+                else if (smode == "write")
+                {
+                    string mode = "W";
+
+                    Console.WriteLine("Content to write : ");
+                    string content = Console.ReadLine();
+
+                    string sent = $"{mode}{sfile}#{content}";
+
+                    client.Send(sent, serverIP);
+                }
+                else if (smode == "append")
+                {
+                    string mode = "A";
+
+                    Console.WriteLine("Content to append : ");
+                    string content = Console.ReadLine();
+
+                    string sent = $"{mode}{sfile}#{content}";
+
+                    client.Send(sent, serverIP);
+                }
+                else
+                {
+                    Console.WriteLine("Unknown mode !");
+                    continue;
+                }
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ FTP_Server/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Total_library.Network;
+using static Total_library.Network.Com;
+
+namespace FTP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            int initialCount = server.got.Count;
+            
+            while (true)
+            {
+                if (server.got.Count > initialCount)
+                {
+                    try
+                    {
+                        ComData cd = server.got[0];
+                        string input = Encoding.ASCII.GetString(cd.Data);
+                        string filename = "";
+                        string content = "";
+                        int? mode = null;
+
+                        if (input[0] == 'W')
+                        {
+                            mode = 1;
+                        }
+                        else if (input[0] == 'R')
+                        {
+                            mode = 2;
+                        }
+                        else if (input[0] == 'A')
+                        {
+                            mode = 3;
+                        }
+                        else
+                        {
+                            server.Send("Error : unknown mode !", cd.Client.Address.ToString());
+                            server.got.RemoveAt(0);
+                            continue;
+                        }
+
+                        input = input.Remove(0, 1);
+
+                        if (mode != 2 && mode != null)
+                        {
+                            for (int i = 0; i < input.Length; i++)
+                            {
+                                if (input[i] == '#' && input[i - 1] != '|')
+                                {
+                                    content = input.Remove(0, i + 1);
+                                    break;
+                                }
+                                else
+                                {
+                                    filename += input[i];
+                                }
+                            }
+                        }
+                        else
+                        {
+                            filename = input;
+                        }
+
+                        if (mode == 1)
+                        {
+                            File.WriteAllText(filename, content);
+                            Console.WriteLine($"{DateTime.Now} => {cd.Client.Address.ToString()} has written in {filename} : {content}");
+                        }
+                        else if (mode == 2)
+                        {
+                            string fileContent = File.ReadAllText(filename);
+                            server.Send($"READCONTENT_{fileContent}", cd.Client.Address.ToString());
+                            Console.WriteLine($"{DateTime.Now} => {cd.Client.Address.ToString()} has read {filename}");
+                        }
+                        else if (mode == 3)
+                        {
+                            File.AppendAllText(filename, content);
+                            Console.WriteLine($"{DateTime.Now} => {cd.Client.Address.ToString()} has appended in {filename} : {content}");
+                        }
+
+                        mode = null;
+                        filename = "";
+                        content = "";
+
+                        server.got.RemoveAt(0);
+                    }
+                    catch
+                    {
+                        server.Send("Error while recepting data !", server.got[0].Client.Address.ToString());
+                        server.got.RemoveAt(0);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
